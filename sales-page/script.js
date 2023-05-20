f844eba2ba89c1b60e7a27b92c4987d09b36775a
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-05-20 16:45:58 +0300

    sales page refactoring

--- sales-page/script.js
+++ sales-page/script.js
@@ -1,14 +1,23 @@
-const questions = document.querySelectorAll('.faq-question');
+const accordion = () => {
 
-questions.forEach((question) => {
-  question.addEventListener('click', function () {
+  const accordionButtons = document.querySelectorAll('.accordion-button');
+
+  accordionButtons.forEach( btn => {
+    btn.addEventListener( 'click', handleAccordionButtonClick );
+  });
+
+  function handleAccordionButtonClick() {
+
     this.classList.toggle('active');
+
     const answer = this.nextElementSibling;
 
-    if (this.classList.contains('active')) {
+    if ( this.classList.contains('active') ) {
       answer.style.maxHeight = answer.scrollHeight + 'px';
     } else {
       answer.style.maxHeight = '0';
     }
-  });
-});
+  }
+};
+
+accordion();
